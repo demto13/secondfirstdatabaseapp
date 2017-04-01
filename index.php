75ebadeedeb078033ab6836f524bbe67a012d902
ToSzi <t.horvath@rgu.ac.uk>
2017-04-01 22:16:10 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ToSzi
+ * Date: 4/1/2017
+ * Time: 10:14 PM
+ */
+
+printf("%n%s", "This will be another trial for an app which will use databases");
+
+?>
